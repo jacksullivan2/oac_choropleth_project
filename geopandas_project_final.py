e39038ce061aff3ed1a38f3d3ada3377564acb0d
jacksullivan2 <44735471+jacksullivan2@users.noreply.github.com>
2021-08-27 16:51:51 +0100

    Add files via upload
    Some extra coding was needed to overcome an issue of LAD's not being plotted if they didn't have a postcode in the client data. So, I added rows to our final geodataframe of these LAD's and gave them 0 values for their market value and policy count. The order of the code was rearranged slightly to achieve this. 
    I realised that there was still a couple of parameters which needed to be defined to make the code completely reusable for different data sets - avoiding the need of the user to change the body of the function for each data set. These changes were made on the final date of my summer internship at OAC.

--- geopandas_project_final.py
+++ geopandas_project_final.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 12 12:22:18 2021
+Created on Fri Aug 27 16:43:41 2021
 
 @author: JackSullivan
 """
 
-def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_column_header, postcode_lad_filepath, shapefile_filepath, weighted_column, weighted_column_type):
+def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_column_header, postcode_lad_filepath, shapefile_filepath, market_value_column_name, policy_count_column_name, weighted_column, weighted_column_type):
     """Produce a choropleth map based on the inputted data"""
     # Import the necessary libraries
     import pandas as pd
     from matplotlib import pyplot as plt
     import geopandas as gpd
     from mpl_toolkits.axes_grid1 import make_axes_locatable
+    
+    ### Use Geopandas library to read in the ONS shapefile into a geodataframe ###
+    map_df = gpd.read_file(shapefile_filepath)
+
+    # Rename the column headers
+    map_df = map_df.rename(index=str, columns={"LAD21NM": "LAD"})
+
 
     # Read in OAC's client data
     try:
@@ -63,6 +70,7 @@ def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_colu
     client_data = client_data.rename(
         index=str, columns={"Postcodes_cleaned": "Postcode"})
 
+    
     # Read in the ONS data which gives the corresponding LAD name to
     # each UK postcode
     postcode_lad = pd.read_csv(postcode_lad_filepath, low_memory=False)
@@ -81,20 +89,20 @@ def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_colu
     # This dataframe will be useful later in extracting the number of unique postcodes 
     # for which we have data for
     a = first_merge.groupby(['Postcode', 'LAD'],
-                            as_index=False)['SumAssured'].sum()
+                            as_index=False)[market_value_column_name].sum()
     
     # Now create a new dataframe from this new dataframe which has the aggregate 
     # Market Value (MV) of the policies held in each LAD. Call this mv_dataframe. 
-    mv_dataframe = a.groupby(['LAD'], as_index=False)['SumAssured'].sum()
+    mv_dataframe = a.groupby(['LAD'], as_index=False)[market_value_column_name].sum()
 
     # Create another new dataframe with the policy count per LAD (the number of
     # policies held by households in each LAD). Call this pc_dataframe
     policy_count = first_merge['LAD'].value_counts()
-    count_dict = {'LAD': [], 'PolicyCount': []}
+    count_dict = {'LAD': [], policy_count_column_name: []}
 
     for lad, count in policy_count.items():
         count_dict['LAD'].append(lad)
-        count_dict['PolicyCount'].append(count)
+        count_dict[policy_count_column_name].append(count)
 
     pc_dataframe = pd.DataFrame(count_dict)
     
@@ -102,18 +110,34 @@ def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_colu
     # gives a dataframe with one row per LAD, with a PolicyCount and MV column. 
     # Name it mv_pc_df
     mv_pc_df = pd.merge(pc_dataframe, mv_dataframe, on='LAD')
-
     
-    # Use Geopandas library to read in the ONS shapefile into a geodataframe
-    map_df = gpd.read_file(shapefile_filepath)
-
-    # Rename the column headers
-    map_df = map_df.rename(index=str, columns={"LAD21NM": "LAD"})
-
     
+    # Now we need to add the LAD's which are missing from mv_pc_df and give 
+    # each of these districts an aggregate market value and policy count of 0.
+    # In doing this we'll create a new dataframe called complete_df
+    all_lad_list = list(map_df['LAD'])
+    
+    # LAD's accounted for in our data 
+    mapped_lads = mv_pc_df['LAD']
+    mapped_lads_list = list(mapped_lads)
+    
+    # Obtain a list for the missing LAD's (the LAD's not in our final_clean_df)
+    missing_lads = [lad for lad in all_lad_list if lad not in mapped_lads_list]
+    
+    a = {'LAD': [], policy_count_column_name: [], market_value_column_name: []}
+    for missing_lad in missing_lads:
+        a['LAD'].append(missing_lad)
+        a[policy_count_column_name].append(0)
+        a[market_value_column_name].append(0)
+            
+        
+    missing_lads_df = pd.DataFrame(a)
+    complete_df = mv_pc_df.append(missing_lads_df, ignore_index=True)
+    
+       
     # Merge our map geodataframe to our dataframe with the MV and PolicyCount
     # columns. 
-    merged_gdf_df = pd.merge(map_df, mv_pc_df, on="LAD")
+    merged_gdf_df = pd.merge(map_df, complete_df, on="LAD")
     
     # Write our final geodataframe to a csv file 
     merged_gdf_df.to_csv("final_geodataframe.csv", mode='w')
@@ -139,7 +163,7 @@ def choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_colu
     ax.set_title(f"UK Postcode Exposure (weighted by {weighted_column})", fontsize=11)
    
     # Set the subtitle 
-    plt.suptitle(f"Data plotted for {len(a)} postcodes in {len(merged_gdf_df['LAD'])} Local Authority Districts", fontsize=7)
+    plt.suptitle(f"Data plotted for {len(series)} postcodes in {len(merged_gdf_df['LAD'])} Local Authority Districts", fontsize=7)
 
     # Remove the axes
     ax.set_axis_off()
@@ -156,10 +180,14 @@ fname = "choropleth_map_sample_data_MV.png"
 client_data_postcode_column_header = "Postcode"
 postcode_lad_filepath = "postcode_to_lad"
 shapefile_filepath = "Local_Authority_Districts_(May_2021)_UK_BUC\Local_Authority_Districts_(May_2021)_UK_BUC.shp"
+market_value_column_name='SumAssured'
+policy_count_column_name='PolicyCount'
 weighted_column='SumAssured'
+
+
 # If the weighted column is a market value, enter 'MV' for the following variable. 
 # or if the weighted column is based on policy count type 'PC' 
 weighted_column_type = 'MV'
 
 
-choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_column_header, postcode_lad_filepath, shapefile_filepath, weighted_column, weighted_column_type)
+choropleth(data_filepath, excel_sheet_name, fname, client_data_postcode_column_header, postcode_lad_filepath, shapefile_filepath, market_value_column_name, policy_count_column_name, weighted_column, weighted_column_type)
